07e39fc22c78575ea6408e11cfd03cfc66cbabcc
Chinh <chinhcaocu@gmail.com>
2024-10-20 14:27:42 +0700

    fixed local storage

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,7 +3,7 @@ import MainLayout from "@/components/MainLayout/MainLayout";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import styles from "./signin.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 
 const LoginPage = () => {
@@ -12,24 +12,28 @@ const LoginPage = () => {
   };
   const { data: session, status } = useSession();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
+  const [loginStatus, setLoginStatus] = useState("");
   useEffect(() => {
-    const existingValue = localStorage.getItem("loginStatus");
-    if (!existingValue) {
-      localStorage.setItem(
-        "loginStatus",
-        "Đăng nhập bằng email đã đăng ký Silencio"
-      );
-    }
+    if (typeof window !== "undefined") {
+      const existingValue = localStorage.getItem("loginStatus");
+      if (!existingValue) {
+        localStorage.setItem(
+          "loginStatus",
+          "Đăng nhập bằng email đã đăng ký Silencio"
+        );
+      }
 
-    if (status === "authenticated") {
-      localStorage.setItem(
-        "loginStatus",
-        "Đăng nhập bằng email đã đăng ký Silencio"
-      );
-      redirect("/");
+      if (status === "authenticated") {
+        localStorage.setItem(
+          "loginStatus",
+          "Đăng nhập bằng email đã đăng ký Silencio"
+        );
+        redirect("/");
+      }
+      setLoginStatus(existingValue!);
     }
   }, [session?.user?.email, status]);
+
   return (
     <MainLayout color="#980000" text="Cổng đăng nhập">
       <button
@@ -40,11 +44,7 @@ const LoginPage = () => {
         <Image src="/google.webp" width={50} height={50} alt="logo" />
         Đăng nhập bằng Google
       </button>
-      <h4 className={styles.status} suppressHydrationWarning>
-        {typeof window !== "undefined"
-          ? localStorage.getItem("loginStatus")
-          : ""}
-      </h4>
+      <h4 className={styles.status}>{loginStatus}</h4>
     </MainLayout>
   );
 };
