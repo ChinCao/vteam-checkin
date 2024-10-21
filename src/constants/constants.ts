bd6c5815092ad152fa9c47b756d270ceb83ab4d3
Chinh <chinhcaocu@gmail.com>
2024-10-21 22:17:01 +0700

    commit

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const concert_date = new Date("2024-10-21T22:20:00+07:00");
+export const concert_date = new Date("2024-10-21T22:27:00+07:00");
 
 export type SpreadsheetData = [
   string, // 'Index 16'
