9fdbf04ce8f7a38a06a476a5ca3d0d1f2f32ebbb
Chinh <chinhcaocu@gmail.com>
2024-10-25 11:29:44 +0700

    finale

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,4 @@
-// export const concert_date = new Date("2024-11-01T19:35:00+07:00");
-export const concert_date = new Date("2024-10-25T11:21:30+07:00");
+export const concert_date = new Date("2024-11-01T19:35:00+07:00");
 
 export type SpreadsheetData = [
   string, // 'Index 16'
